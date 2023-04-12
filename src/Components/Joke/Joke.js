1a5321447e15e92943228b657037fd1904735021
Santiago-pe <penasantiago346@gmail.com>
2023-04-11 23:19:27 -0300

    Creating local storage

--- src/Components/Joke/Joke.js
+++ src/Components/Joke/Joke.js
@@ -1,30 +1,58 @@
 import React, {Component} from "react";
-import { PlusLg, DashLg, EmojiFrown } from "react-bootstrap-icons";
+import { PlusLg, DashLg, EmojiFrown,BoxArrowDown, Trash3 } from "react-bootstrap-icons";
 import Button from 'react-bootstrap/Button';
 
 class Joke extends Component {
+
+    static defaultProps = {
+        tabNewJokes: "newJokes",
+        tabMyJokes: "myJokes"
+    }
     constructor(props){
         super(props)
     }
     render(){
         // Destructuring props
-        const {vote, text, upVote, downVote} = this.props
+        const {vote, text, upVote, downVote, saveJoke, activeTab, deletJoke, tabNewJokes, tabMyJokes} = this.props;
+        
         return(
             <tr>
                 <td>
                     <div className="d-flex justify-content-around aling-items-center">
-                        <Button variant="outline-dark" size="sm" onClick={upVote}>    
-                            <PlusLg/>
-                        </Button>
+                            {
+                                activeTab === tabNewJokes && 
+                                <Button variant="outline-dark" size="sm" onClick={upVote}>    
+                                    <PlusLg/>
+                                </Button>
+                            }
                             {vote}
-                        <Button variant="outline-dark" size="sm" onClick={downVote}>
-                            <DashLg/>
-                        </Button>   
+                            {
+                                activeTab === tabNewJokes &&
+                                <Button variant="outline-dark" size="sm" onClick={downVote}>
+                                    <DashLg/>
+                                </Button>   
+                            }
                     </div> 
                 </td>
                 <td>{text}</td>
                 <td>
-                    <EmojiFrown/>
+                    <div className="d-flex justify-content-center aling-items-center">
+                        <EmojiFrown/>
+                    </div>
+                </td>
+                <td>
+                    <div className="d-flex justify-content-center aling-items-center">
+                        {activeTab === tabMyJokes ?
+                        <Button variant="outline-dark" size="sm" onClick={deletJoke} >
+                            <Trash3 />
+                        </Button>
+                        :
+                       
+                        <Button variant="outline-dark" size="sm" onClick={saveJoke} >
+                            <BoxArrowDown />
+                        </Button>
+                        }
+                    </div>
                 </td>
             </tr>
         )
--- src/Components/JokeList/JockeList.js
+++ src/Components/JokeList/JockeList.js
@@ -11,20 +11,28 @@ import { v4 as uuidv4 } from 'uuid';
 
 class JokesList extends Component {
     static defaultProps = {
-        numJokesToGet : 10
+        numJokesToGet : 10,
     }
     constructor(props){
         super(props);
         this.state = {
             jokes: [],
-            activeNewJokesTab: "myJokes" 
+            jokesStorage: JSON.parse(window.localStorage.getItem("jokes")) || [],
+            activeTab: "myJokes"
         }
         //Bind Functions
-        //this.handleVote = this.handleVote.bind(this)
+        this.handleClick = this.handleClick.bind(this);
+        this.getJokes = this.getJokes.bind(this)
     }
 
     /* ---------- API CALL ---------- */
-    async componentDidMount (){
+     componentDidMount (){
+       this.getJokes()
+        
+    }
+
+    /* ---------- Functions ---------- */
+    async getJokes () {
         // Current Jokes
         let currJokes = [];
         // The api call will be executed 10 times
@@ -37,10 +45,9 @@ class JokesList extends Component {
             currJokes.push({id: uuidv4() , joke: res.data.joke, votes:0}) 
         }
         // Set state
-        this.setState({jokes: currJokes})
+        this.setState(st => ({jokes: [... st.jokes, ...currJokes]}))
     }
 
-    /* ---------- Functions ---------- */
     handleVote (id, delta){
         this.setState(
             st => ({
@@ -51,6 +58,55 @@ class JokesList extends Component {
         )
     }
 
+    handleClick () {
+        // Cambio a mi ventana de chistes
+        this.setState({activeTab: "newJokes"});
+        // Traigo 10 chiste nuevos
+        this.getJokes();
+        
+    }
+
+    deletJoke (id) {
+        // Obtener los datos almacenados en Local Storage
+        const jokesStorage = JSON.parse(localStorage.getItem('jokes'));
+
+        // Eliminar el chiste del array de chistes
+        const updatedJokesStorage = jokesStorage.filter(joke => joke.id !== id);
+
+        // Actualizar Local Storage con los nuevos datos
+        localStorage.setItem('jokes', JSON.stringify(updatedJokesStorage));
+
+        // Actualizar el estado con los nuevos datos
+        this.setState({ jokesStorage: updatedJokesStorage });
+        
+    }
+
+    saveJoke(id) {
+        // Desestructuro mi estado
+        const { jokes, jokesStorage } = this.state;
+
+        // Capturo el chiste que deseo guardar
+        const jokeToSave = jokes.find(j => j.id === id);
+      
+        // Pregunto si ya lo tengo en el estado
+
+        if (jokesStorage.some(j => j.joke === jokeToSave.joke)) {
+            alert("This joke is already saved!");
+            return;
+        }
+        
+        // Si no lo tengo porcedo a guardarlo en mi en una constante jutno con mis otros chistes almacenados
+        const updatedJokesStorage = [...jokesStorage, jokeToSave];
+        // Los guardo en mi local storages
+        window.localStorage.setItem("jokes", JSON.stringify(updatedJokesStorage));
+        
+        // Y seteo el estado para poder mostrarlos
+        this.setState({
+          jokesStorage: updatedJokesStorage
+        });
+
+        console.log("Se guardo mi chiste")
+    }
     /* ---------- Render Component ---------- */
     render(){
         //const listJokes = null;
@@ -59,16 +115,18 @@ class JokesList extends Component {
             <Container fluid="md" className="bg-light p-2 mt-5">
                 <div className="d-flex justify-content-start align-items-center my-3 p-2">
                     <h2 className="me-4">App Joke</h2>
-                    <Button variant="warning" className="ml-1">Get jokes</Button>
+                    <Button variant="warning" className="ml-1" onClick={this.handleClick}>Get jokes</Button>
                 </div>
                 
                 <div  className="bg-white p-2">
                     <Tabs
-                    defaultActiveKey={this.state.activeNewJokesTab}
+                    activeKey={this.state.activeTab}
+                    onSelect={(tab) => this.setState({activeTab: tab})}
                     id="uncontrolled-tab-example"
                     className="mb-3"
+                    
                     >
-                        <Tab eventKey="newJokes" title="New Jokes">
+                        <Tab eventKey="newJokes" title="New Jokes" style={{ overflowY: "scroll", maxHeight: "520px" }}>
                         
                             <Table striped bordered hover>
                                 <thead>
@@ -76,6 +134,7 @@ class JokesList extends Component {
                                     <th>Clasification</th>
                                     <th>Jokes</th>
                                     <th>Raiting</th>
+                                    <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -87,15 +146,44 @@ class JokesList extends Component {
                                             vote={j.votes} 
                                             text={j.joke} 
                                             upVote={() => this.handleVote(j.id, 1)}
-                                            downVote={() => this.handleVote(j.id, -1)}/>
+                                            downVote={() => this.handleVote(j.id, -1)}
+                                            saveJoke={() => this.saveJoke(j.id)}
+                                            activeTab = {this.state.activeTab}    
+                                            />
                                         ))
                                     }
                                 </tbody>
                             </Table>
                         </Tab>
                     
-                        <Tab eventKey="myJokes" title="My Jokes">
+                        <Tab eventKey="myJokes" title="My Jokes" style={{ overflowY: "scroll", maxHeight: "520px" }}>
                             <h3>You didn't have jokes yet</h3>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                    <th>Clasification</th>
+                                    <th>Jokes</th>
+                                    <th>Raiting</th>
+                                    <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        //Pass as props the data to chield component
+                                        this.state.jokesStorage.map((j, idx) => ( 
+                                            <Joke 
+                                            key={idx} 
+                                            vote={j.votes} 
+                                            text={j.joke} 
+                                            upVote={() => this.handleVote(j.id, 1)}
+                                            downVote={() => this.handleVote(j.id, -1)}
+                                            deletJoke={() => this.deletJoke(j.id)}
+                                            activeTab = {this.state.activeTab}    
+                                            />
+                                        ))
+                                    }
+                                </tbody>
+                            </Table>
                         </Tab>
 
                     </Tabs>
